863a8d1ca13c6374bbd3f1c95ca54c63467e354c
christoffervalland <christoffervalland@gmail.com>
2015-02-23 14:28:17 +0100

    Added spidered urls and a loading bar
    As the title says

--- Recommending/presentation.js
+++ Recommending/presentation.js
@@ -2,10 +2,12 @@
 Get ten objects from Mongolab DB.
 Print these ten to the objectlist
 
-To-do: Sort the ten collected objects by highest value on DB
+To-do: Check if spiderurls exists in popular urls
+		IF exists: Total value = spider.weight + url.weight
 Why: So that the most popular object will be printed first.
 **/
 function getTenObjects(){
+	$('#objectLoading').show();
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", 'https://api.mongolab.com/api/1/databases/testbase/collections/objects?apiKey=2P7QlEw29SmcG6BrJ5TZJZZT-eQmd64s');
 	xhr.onload = function(){
@@ -18,6 +20,8 @@ function getTenObjects(){
 				//i+1 because it's an array and I don't want the first object to be 0.
 				printObjects(i+1, tempObject.collectedobject.object, tempObject.collectedobject.weight);
 			}
+			$('#objectLoading').hide();
+
 		}
 	xhr.send();
 }
@@ -29,10 +33,11 @@ function printObjects(value, object, popularity){
 }
 
 function getTenUrls(){
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", 'https://api.mongolab.com/api/1/databases/testbase/collections/urls?apiKey=2P7QlEw29SmcG6BrJ5TZJZZT-eQmd64s');
-	xhr.onload = function(){
-			var response = JSON.parse(xhr.responseText);
+	$('#urlLoading').show();
+	var xhrUrl = new XMLHttpRequest();
+	xhrUrl.open("GET", 'https://api.mongolab.com/api/1/databases/testbase/collections/urls?apiKey=2P7QlEw29SmcG6BrJ5TZJZZT-eQmd64s');
+	xhrUrl.onload = function(){
+			var response = JSON.parse(xhrUrl.responseText);
 			response.sort(function(a, b){
 				return b.visited.weight - a.visited.weight;
 			});
@@ -41,12 +46,37 @@ function getTenUrls(){
 				//i+1 because it's an array and I don't want the first object to be 0.
 				printUrls(i+1, tempUrl.visited.url, tempUrl.visited.weight);
 			}
+			$('#urlLoading').hide();
 		}
-	xhr.send();
+	xhrUrl.send();
 }
 getTenUrls();
 
 function printUrls(value, url, popularity){
 	var htmlList = $("#urlList");
 	htmlList.append('<li>' + value + ': ' + url + " | " + popularity + '</li>');
+}
+
+function getTenSpiders(){
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", 'https://api.mongolab.com/api/1/databases/testbase/collections/spiderurls?apiKey=2P7QlEw29SmcG6BrJ5TZJZZT-eQmd64s');
+	xhr.onload = function(){
+			var response = JSON.parse(xhr.responseText);
+			response.sort(function(a, b){
+				return b.spider.weight - a.spider.weight;
+			});
+			for(var i = 0; i < 10; i++){
+				var tempUrl = response[i];
+				//i+1 because it's an array and I don't want the first object to be 0.
+				printSpiders(i+1, tempUrl.spider.url, tempUrl.spider.weight);
+			}
+			$('#spiderLoading').hide();
+		}
+	xhr.send();
+}
+getTenSpiders();
+
+function printSpiders(value, url, popularity){
+	var spiderList = $('#spiderurls');
+	spiderList.append('<li>' + value + ': ' + url + " | " + popularity + '</li>')
 }
